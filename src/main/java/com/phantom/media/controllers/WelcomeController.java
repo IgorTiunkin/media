948a6f01416938e2b683074ae4f4daf14fed6d58
Igor Tiunkin <fantomenergy@gmail.com>
2023-12-05 17:10:23 +0300

    base commit

---
+++ src/main/java/com/phantom/media/controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.phantom.media.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping()
+    public String index() {
+        return "/welcome/welcome";
+    }
+}
